42eb7483b2a9722b12183a09511b106ceb720e4b
gtaigniere <54893913+gtaigniere@users.noreply.github.com>
2023-07-20 08:01:46 +0200

    initial commit

---
+++ backend/src/main/java/com/example/ww2germansubmarines/Ww2GermanSubmarinesApplication.java
@@ -0,0 +1,14 @@
+package com.example.ww2germansubmarines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ww2GermanSubmarinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ww2GermanSubmarinesApplication.class, args);
+
+	}
+
+}
---
+++ backend/src/test/java/com/example/ww2germansubmarines/Ww2GermanSubmarinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ww2germansubmarines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ww2GermanSubmarinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
