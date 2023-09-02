9bc780bfc8c03dabd7d12348cf747c082348d0ba
gtaigniere <54893913+gtaigniere@users.noreply.github.com>
2023-09-02 17:53:02 +0200

    Feature : Mise en place de la gestion des erreurs en back

---
+++ backend/src/main/java/com/example/ww2germansubmarines/core/exceptions/Ww2gsException.java
@@ -0,0 +1,14 @@
+package com.example.ww2germansubmarines.core.exceptions;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class Ww2gsException extends RuntimeException {
+    private final HttpStatus httpStatus;
+
+    public Ww2gsException(RaisonEnum raison, HttpStatus httpStatus) {
+        super(raison.getMessage());
+        this.httpStatus = httpStatus;
+    }
+}
---
+++ backend/src/main/java/com/example/ww2germansubmarines/core/rest/dtos/ErreurReponse.java
@@ -0,0 +1,10 @@
+package com.example.ww2germansubmarines.core.rest.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@AllArgsConstructor
+@Builder
+public class ErreurReponse {
+    private String message;
+}
